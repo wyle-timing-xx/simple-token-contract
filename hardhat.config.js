76b8db8b6911f7bda1a402c65316f8ff3d83ffa4
wyle-timing-xx <wyle-timing@outlook.com>
2025-09-27 09:13:55 +0800

    Update hardhat config for Sepolia deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,10 +19,18 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8545"
     },
-    // 可以添加其他网络配置
-    // sepolia: {
-    //   url: process.env.SEPOLIA_URL || "",
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "https://sepolia.infura.io/v3/your_infura_project_id",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+      gasPrice: "auto",
+      gas: "auto"
+    },
+    // 可以添加其他网络
+    // mainnet: {
+    //   url: process.env.MAINNET_URL || "",
     //   accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    //   chainId: 1,
     // },
   },
   gasReporter: {
